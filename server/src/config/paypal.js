dd7a64eacd80e0db99fa181b869d73fde51c1fcb
netanelNM <156360377+netanelNM@users.noreply.github.com>
2024-12-29 20:58:50 +0200

    add paypal payment meted

--- server/src/config/paypal.js
+++ server/src/config/paypal.js
@@ -1,6 +1,11 @@
-import {Client,OrdersController} from "@paypal/paypal-server-sdk"
+import {Client,OrdersController,PaymentsController} from "@paypal/paypal-server-sdk"
+import "dotenv/config";
+
 // import Paypal from "@paypal/paypal-server-sdk"
 // const client = new Paypal.Client({
+console.log("PAYPAL_ID",process.env.PAYPAL_ID);
+console.log("PAYPAL_SECRET",process.env.PAYPAL_SECRET);
+
 const paypalClient = new Client({
     clientCredentialsAuthCredentials: {
       oAuthClientId: process.env.PAYPAL_ID,
@@ -9,7 +14,8 @@ const paypalClient = new Client({
 });
 
 const paypal = {
-  orders: new OrdersController(paypalClient)
+  orders: new OrdersController(paypalClient),
+  payments: new PaymentsController(paypalClient)
 }
 
 export default paypal
