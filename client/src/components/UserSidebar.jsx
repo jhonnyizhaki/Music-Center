49e4b04bb195240b6054e8d8e961baf17f036e73
netanelNM <156360377+netanelNM@users.noreply.github.com>
2025-02-03 17:21:28 +0200

    fix: fixing the logout bottom

--- client/src/components/UserSidebar.jsx
+++ client/src/components/UserSidebar.jsx
@@ -1,13 +1,17 @@
-import React from 'react';
-import { Drawer, List, ListItem, ListItemText, Typography } from '@mui/material';
-import { NavLink } from 'react-router-dom';
-import '../AdminSidebar.css';
+import React from "react"
+import { Drawer, List, ListItem, ListItemText, Typography } from "@mui/material"
+import { NavLink } from "react-router-dom"
+import "../AdminSidebar.css"
+import axios from "axios"
+import urls from "../constant/URLS"
 
 const UserSidebar = ({ isOpen, closeSidebar }) => {
-  const handleLogout = () => {
-    localStorage.removeItem('user');
-    window.location.href = '/login';
-  };
+  const handleLogout = async () => {
+    await axios.post(urls.LOGOUT)
+    localStorage.removeItem("user")
+
+    window.location.href = "/login"
+  }
 
   return (
     <Drawer anchor="left" open={isOpen} onClose={closeSidebar}>
@@ -19,10 +23,20 @@ const UserSidebar = ({ isOpen, closeSidebar }) => {
           <ListItem button component={NavLink} to="/" onClick={closeSidebar}>
             <ListItemText primary="Home" />
           </ListItem>
-          <ListItem button component={NavLink} to="/user-orders" onClick={closeSidebar}>
+          <ListItem
+            button
+            component={NavLink}
+            to="/user-orders"
+            onClick={closeSidebar}
+          >
             <ListItemText primary="Orders" />
           </ListItem>
-          <ListItem button component={NavLink} to="/contact" onClick={closeSidebar}>
+          <ListItem
+            button
+            component={NavLink}
+            to="/contact"
+            onClick={closeSidebar}
+          >
             <ListItemText primary="contact" />
           </ListItem>
           <ListItem button onClick={handleLogout}>
@@ -31,7 +45,7 @@ const UserSidebar = ({ isOpen, closeSidebar }) => {
         </List>
       </div>
     </Drawer>
-  );
-};
+  )
+}
 
-export default UserSidebar;
+export default UserSidebar
