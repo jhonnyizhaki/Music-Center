f7bceed5b46063684cc2dbbf3c342db4c0701f44
Yehonatan <creatoryry@gmail.com>
2025-02-09 13:33:25 +0200

    edit admin products

--- client/src/pages/admin/AdminProducts.jsx
+++ client/src/pages/admin/AdminProducts.jsx
@@ -41,8 +41,7 @@ const AdminProducts = () => {
     try {
       const response = await axios.get(urls.INSTRUMENTS)
       setProducts(response.data)
-      console.log("res.data", response.data);
-      
+      console.log("res.data", response.data)
     } catch (error) {
       console.error("Error fetching products:", error)
     }
@@ -126,7 +125,11 @@ const AdminProducts = () => {
 
   return (
     <Box sx={{ height: 600, width: "100%", p: 3, mt: 8 }}>
-      <Typography variant="h5" gutterBottom sx={{ color: "white" , mb: 4, textAlign: "center" }}>
+      <Typography
+        variant="h5"
+        gutterBottom
+        sx={{ color: "white", mb: 4, textAlign: "center" }}
+      >
         <h1>Manage Products</h1>
       </Typography>
       <Button
@@ -144,7 +147,8 @@ const AdminProducts = () => {
         <DataGrid
           rows={products}
           columns={columns}
-          getRowId={(row) => row._id || row.name + row.price}          pageSizeOptions={[5, 10, 25]}
+          getRowId={(row) => row._id || row.name + row.price}
+          pageSizeOptions={[5, 10, 25]}
           disableRowSelectionOnClick
         />
       </Box>
